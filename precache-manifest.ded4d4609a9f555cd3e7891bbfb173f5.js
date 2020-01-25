53988aae06d90d29716333f261370487b1ac7c9d
Fred <frederic@pmo.digital>
2020-01-25 07:37:01 +0100

    Updates

---
+++ precache-manifest.ded4d4609a9f555cd3e7891bbfb173f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16792935da6b4ca13fa02cfb21a62f8b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5c1ad331f13ca0575ea5",
+    "url": "/todolist/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5c1ad331f13ca0575ea5",
+    "url": "/todolist/static/js/2.e44c9c03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.e44c9c03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3a88558bebbadafff0a",
+    "url": "/todolist/static/js/main.a1d8387d.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
